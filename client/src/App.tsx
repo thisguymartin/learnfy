74030ba889ccce934cfcb9024c70177138ff2817
Martin Patino <martin@sibipro.com>
2022-09-12 16:57:20 -0700

    chore: added cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  
+  useEffect(() => {
+    axios.get('http://localhost:5288/api/Courses').then((response) => {
+      console.log(response);
+      setCourses(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <ul>
+        {courses.map((course: any, index) => {
+          return (
+            <li key={index}>
+              
+              <div>
+              <img src={course.image} height="80px"></img>
+
+
+            </div>
+              <p>{course.title}</p>
+
+            </li>
+          );
+        })}
+      </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
